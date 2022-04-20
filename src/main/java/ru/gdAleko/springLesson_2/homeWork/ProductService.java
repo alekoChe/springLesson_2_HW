ca4e9f7d5e82bc6f7ba68d5346d1b6c41f4a54d6
alekoChe <aleko2117bis@gmail.com>
2022-04-20 13:23:24 +0300

    9

--- src/main/java/ru/gdAleko/springLesson_2/homeWork/ProductService.java
+++ src/main/java/ru/gdAleko/springLesson_2/homeWork/ProductService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 
 @Component()
-public class ProductService { //////////////////////////
+public class ProductService { //////////////////////////.
 
     @Autowired
     private Repository repository;
